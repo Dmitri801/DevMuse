c09bd069380cfed800067b9bf7b6d7a949167c80
Dmitri <dsharsh@hotmail.com>
2018-08-11 22:17:51 -0600

    updated color loss

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,11 +1,14 @@
 // FUNCTIONAL COMPONENT - shortkey = rfc *TAB*
 
-import React from 'react'
+import React from "react";
 
 export default () => {
   return (
-    <footer className="bg-dark text-white mt-5 p-4 text-center">
+    <footer
+      style={{ backgroundColor: "#000" }}
+      className="text-white mt-5 p-4 text-center"
+    >
       Copyright &copy; {new Date().getFullYear()} DevMuse
     </footer>
-  )
-}
+  );
+};
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,29 +1,34 @@
 // CLASS BASED COMPONENT - short key = rcc *TABKEY*
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
-import { PropTypes } from 'prop-types';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { PropTypes } from "prop-types";
+import { connect } from "react-redux";
 
 class Landing extends Component {
   componentDidMount() {
     if (this.props.auth.isAuthenticated) {
-      this.props.history.push('/dashboard');
+      this.props.history.push("/dashboard");
     }
   }
   render() {
-    return <div>
+    return (
+      <div>
         <div className="landing">
           <div className="dark-overlay landing-inner text-light">
             <div className="container">
               <div className="row">
                 <div className="col-md-12 text-center">
                   <h1 className="display-3 mb-4">DevMuse</h1>
-                  <p className="lead">
+                  <p style={{ color: "#fff" }} className="lead">
                     {" "}
-                    A social network for developers in the music industry, create a profile, share your work, and get inspired..
+                    A social network for developers in the music industry,
+                    create a profile, share your work, and get inspired..
                   </p>
                   <hr />
-                  <Link to="/register" className="btn btn-lg btn-outline-info mr-2">
+                  <Link
+                    to="/register"
+                    className="btn btn-lg btn-outline-info mr-2"
+                  >
                     Sign Up
                   </Link>
                   <Link to="/login" className="btn btn-lg btn-outline-light">
@@ -34,16 +39,17 @@ class Landing extends Component {
             </div>
           </div>
         </div>
-      </div>;
+      </div>
+    );
   }
 }
 
 Landing.propTypes = {
   auth: PropTypes.object.isRequired
-}
+};
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   auth: state.auth
-})
+});
 
 export default connect(mapStateToProps)(Landing);
